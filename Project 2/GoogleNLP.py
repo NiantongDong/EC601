fcd70f875e7993b3e68240bf010a49e6b018360b
NiantongDong <niantongdong@gmail.com>
2020-09-27 20:00:06 -0400

    GoogleNLP analysis and result

--- Project 2/GoogleNLP.py
+++ Project 2/GoogleNLP.py
@@ -2,18 +2,23 @@
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import json
 
 # Instantiates a client
 client = language.LanguageServiceClient()
 
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
-
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+#Import Tweets result file.
+with open("Hashtag_Tweets.json") as f:
+    data = json.loads(f.read()) 
+Sentiment_output = open("Hashtag_sentiment.txt","w")
+#Analyze each tweet text
+for tweet in data:
+    text = tweet['text']
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    #Call API to analyze text.
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    #Write result to file
+    Sentiment_output.write("Text:" + text +"\n")
+    Sentiment_output.write("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude)+"\n")
